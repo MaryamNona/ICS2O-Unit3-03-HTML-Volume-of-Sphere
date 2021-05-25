7e9d3fd58d727610cbd75bf65fb378a93437693c
MaryamNona <>
2021-05-25 00:07:55 +0000

    added index.html

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # ICS2O-Unit3-03-HTML-Volume-of-Sphere
+
+[![GitHub's Super Linter](https://github.com/MaryamNona/ICS2O-Unit3-03-HTML-Volume-of-Sphere/workflows/GitHub's%20Super%20Linter/badge.svg)](https://github.com/MaryamNona/ICS2O-Unit3-03-HTML-Volume-of-Sphere/actions)
+
+This website can be found at: [https://MaryamNona.github.io/ICS2O-Unit3-03-HTML-Volume-of-Sphere](https://MaryamNona.github.io/ICS2O-Unit3-03-HTML-Volume-of-Sphere)
---
+++ js/script.js
@@ -0,0 +1,20 @@
+// Copyright (c) 2020 Mr. Coxall All rights reserved
+//
+// Created by: Maryam Nona
+// Created on: May 2021
+// This file contains the JS functions for index.html
+
+"use strict"
+
+function addAgeClicked () {
+  // this function does area 
+
+  // input
+  const length = parseFloat(document.getElementById("radius-of-sphere").value)
+
+  // process
+  const volume = (4 / 3) * 3.14 * radius * radius * radius
+
+  // output
+  document.getElementById('volume').innerHTML = 'Volume is: ' + volume + " mm³"
+}
